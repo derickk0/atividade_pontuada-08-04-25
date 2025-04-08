8802a2294bbb1734665d699a368aba92e2370c5d
Erick Breno <159458075+derickk0@users.noreply.github.com>
2025-04-08 11:13:19 -0300

    Delete atividade_pontuada directory

--- atividade_pontuada/src/main/java/com/aula/atividade_pontuada/repository/FuncionarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aula.atividade_pontuada.repository;
-
-import com.aula.atividade_pontuada.model.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-    boolean existsByEmail(String email);
-}
